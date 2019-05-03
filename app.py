a84b9964ba03ecd9c0f1363ee1467ca53861d418
Nikhil Wagh <nikhilwagh.work@gmail.com>
2019-05-03 21:20:13 +0530

    Updated getNowShowing to add support to query Languages and Dimensions

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, redirect, abort, request, jsonify
-from requests import get
 from bs4 import BeautifulSoup
+from flask import Flask, abort, request, jsonify
+from requests import get
+from json import loads
 
 app = Flask(__name__)
+app.config["JSON_SORT_KEYS"] = False
 
 baseUrl = "https://in.bookmyshow.com"
+
 
 @app.route('/')
 def hello_world():
@@ -17,14 +20,16 @@ def pageNotFound(e):
 	return "Error 404, Page not found."
 
 
-@app.route('/<city>/', methods=["GET"])
+@app.route('/<city>/', methods=["GET", "POST"])
 def getNowShowing(city):
-	event = "movies"
-	if 'event' in request.args:
-		event = request.args.get('event')
-	# dimensions = []
+	# event = request.form.get('event')
+	# if event is None:
+	# 	event = "movies"
+	event = "Movies"
+	data = loads(request.data)
+	dimensions = data['dimensions']
+	languages = data['languages']
 	url = "https://in.bookmyshow.com/{city}/{event}".format(city=city.lower(), event=event.lower())
-	print(url)
 	try:
 		bmsNowShowingContent = get(url)
 	except Exception as e:
@@ -34,7 +39,6 @@ def getNowShowing(city):
 		# print(bmsNowShowingContent.reason)
 		if bmsNowShowingContent.status_code == 404:
 			abort(404, "City/Event not supported")
-		# raise ValueError('City not supported')
 		else:
 			soup = BeautifulSoup(bmsNowShowingContent.text, 'html5lib')
 			movieUrls = {}
@@ -44,15 +48,19 @@ def getNowShowing(city):
 					'name': img['alt'],
 					'booking_url': tag.find('a', recursive=True)['href'],
 					'image_url': img['data-src'],
-					'language': tag['data-language-filter'][1:].split('|'),
+					'languages': tag['data-language-filter'][1:].split('|'),
 					'dimensions': tag['data-dimension-filter'][1:].split('|')
 				}
+				if len(dimensions) > 0:
+					if not any(dimen in movie['dimensions'] for dimen in dimensions):
+						continue
+				if len(languages) > 0:
+					if not any(lang in movie['languages'] for lang in languages):
+						continue
 				movieUrls[movie['name']] = movie
 			if len(movieUrls) <= 0:
 				raise Exception('Data not received from BookMyShow.com')
 			return jsonify(movieUrls)
-
-
 
 
 if __name__ == '__main__':
